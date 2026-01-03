4e3d1be0ca8c6efa0ece186c2e1ea32d9a29ae6b
endikaintxaurtieta <endika@gizaki.com>
2026-01-03 19:42:09 +0100

    Remove 'peer' property from multiple dependencies in package-lock.json and add two input fields with state management to App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+  const [uno, setUno] = useState("init");
+  const [dos, setDos] = useState("init");
+
+  return (
+    <>
+      <input type="text" placeholder="Escribe algo" value={uno} onChange={(e) => setUno(e.target.value)} />
+      <input type="text" placeholder="Escribe algo" value={dos} onChange={(e) => setDos(e.target.value)} />
+    </>
+  )
 }
 
 export default App

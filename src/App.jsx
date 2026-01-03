6160d6595df7e6c65e127531bc74a9ece6106d3a
endikaintxaurtieta <endika@gizaki.com>
2026-01-03 19:51:24 +0100

    loquesea

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import './App.css'
 
 function App() {
   const [uno, setUno] = useState("init");
-  const [dos, setDos] = useState("init");
 
   return (
     <>
       <input type="text" placeholder="Escribe algo" value={uno} onChange={(e) => setUno(e.target.value)} />
-      <input type="text" placeholder="Escribe algo" value={dos} onChange={(e) => setDos(e.target.value)} />
     </>
   )
 }
